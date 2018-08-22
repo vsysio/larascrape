24f78825d4a999ec62a418c210d22ca597a39309
BrandonGould <brandon@vsysio.net>
2018-08-22 11:47:22 -0400

    Added preliminary scraping for Nordstrom: Scrapes a pre-configured list (set in collections) and outputs the results

--- app/Console/Commands/scrapeNordstrom.php
+++ app/Console/Commands/scrapeNordstrom.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use Goutte\Client;
 use Illuminate\Console\Command;
 
 class scrapeNordstrom extends Command
@@ -11,14 +12,24 @@ class scrapeNordstrom extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'scrape:nordstrom';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Scrapes a random page on Nordstrom';
+
+    /**
+     * @var array Collections :: Stored array of collections (ie. https://shop.nordstrom.com/c/{collection}) to scrape
+     */
+    protected $collections = [
+        'womens-activewear-shop',
+        'womens-coats',
+        'womens-dresses-shop',
+        'womens-jeans-shop'
+    ];
 
     /**
      * Create a new command instance.
@@ -38,5 +49,47 @@ class scrapeNordstrom extends Command
     public function handle()
     {
         //
+
+        $this->info("Collections to scrape: ");
+        foreach ($this->collections as $collection) {
+            $this->info("$collection");
+        }
+
+        $this->line("");
+        $this->info('Starting scraping process...');
+
+        foreach ($this->collections as $collection) {
+            $this->scrape($collection);
+        }
+
+    }
+
+    public function scrape($collection) {
+
+        $this->info("Targeting: {$collection}");
+        $this->line("-------------------------------------------------");
+
+        sleep(3);
+
+        $client = new Client();
+
+
+
+            $this->info("Retrieving page... ");
+            $crawler = $client->request('GET', env('NORDSTROM_URL') . "/{$collection}");
+
+            $crawler->filter('div[class*="galleryItem"]')->each(function ($node) {
+
+                $title = $node->filter('h3[class*="title"]');
+
+                if ($title->count()) { // Node error,  skip this one
+
+                    $title = $title->text();
+                    $this->info("{$title}");
+                }
+
+            });
+
+
     }
 }
